6adedb9f477fd0b8596f01c621e949739fd6daf9
clara-delannoy <73298879+clara-delannoy@users.noreply.github.com>
2026-04-14 21:46:59 +0000

    fini

--- app.py
+++ app.py
@@ -12,13 +12,24 @@ print('ventes-par-région.html généré avec succès !')
 
 # 6.A Graphique de ventes par produit 
 ventes_par_produit = données.groupby('produit')['qte'].sum().reset_index()
+#groupby('produit') : regroupe les données par produit mais les valeurs de produit sont en index
+#['qte'].sum() : calcule la somme des quantités vendues pour chaque produit
+#reset_index() : réinitialise l'index pour obtenir un tableau propre  
 figure = px.bar(ventes_par_produit, x='produit', y='qte', title='quantité vendue par produit')
+#px.bar() : crée un graphique à barres avec les données de ventes_par_produit, en utilisant 'produit' pour l'axe x et 'qte' pour l'axe y, et en définissant le titre du graphique
 figure.write_html('ventes-par-produit.html')
+#write_html() : enregistre le graphique dans un fichier HTML nommé 'ventes-par-produit.html'
 print('ventes-par-produit.html généré avec succès !')
 
 # 6.B Graphique du chiffre d'affaires par produit
 données['ca'] = données['prix'] * données['qte']
+#création d'une nouvelle colonne 'ca' dans le tableau données, calculée en multipliant les colonnes 'prix' et 'qte' pour chaque ligne. Cela représente le chiffre d'affaires généré par chaque vente.
 chiffre_affaires_par_produit = données.groupby('produit')['ca'].sum().reset_index()
+#groupby('produit') : regroupe les données par produit
+#['ca'].sum() : calcule la somme du chiffre d'affaires pour chaque produit
+#reset_index() : réinitialise l'index pour obtenir un tableau propre
 figure = px.bar(chiffre_affaires_par_produit, x='produit', y='ca', title='chiffre d\'affaires par produit')
+#px.bar() : crée un graphique à barres avec les données de chiffre_affaires_par_produit, en utilisant 'produit' pour l'axe x et 'ca' pour l'axe y, et en définissant le titre du graphique
 figure.write_html('chiffre-affaires-par-produit.html')
+#write_html() : enregistre le graphique dans un fichier HTML nommé 'chiffre-affaires-par-produit.html'
 print('chiffre-affaires-par-produit.html généré avec succès !')
